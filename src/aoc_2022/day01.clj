0f6114de88117b037dcc7e876c5725792e275fcb
nchapon <nchapon@gmail.com>
2022-12-01 23:32:30 +0100

    Day 01 Part 1

--- src/aoc_2022/day01.clj
+++ src/aoc_2022/day01.clj
@@ -1,22 +1,51 @@
 (ns aoc-2022.day01
-  "FIXME: my new org.corfield.new/scratch project.")
+  "AOC 2022 Day 01"
+  (:require
+   [clojure.string :as string]))
 
-(defn exec
-  "Invoke me with clojure -X aoc-2022.aoc-2022/exec"
-  [opts]
-  (println "exec with" opts))
-
-(defn -main
-  "Invoke me with clojure -M -m aoc-2022.aoc-2022"
-  [& args]
-  (println "-main with" args))
-
-(def calories ["1" "2" "3" "" "1" "2"])
-
-(defn get-calories
-  "Get Calories"
+(defn get-elves-calories
+  "Get Elves Calories"
   []
-  (clojure.string/split-lines (slurp "input/day01.txt")))
+  (string/split-lines (slurp "input/day01.txt")))
+
+(defn- sum-calories
+  [calories]
+  (apply + (map #(Integer/parseInt %) calories)))
+
+(defn max-calories
+  "Get Elfe max calories"
+  [elfes-calories]
+  (reduce max (map sum-calories
+                   (filter #(not (= [""] %))
+                           (partition-by #(.equals "" %) elfes-calories)))))
+
+(comment
+  (def calories
+    ["1000"
+     "2000"
+     "3000"
+     ""
+     "4000"
+     ""
+     "5000"
+     "6000"
+     ""
+     "7000"
+     "8000"
+     "9000"
+     ""
+     "10000"])
+
+  (max-calories calories)
+;; => 24000
+  (max-calories (get-elves-calories))
+;; => 75501
+  )
+
+
+
+
+
 
 
 
